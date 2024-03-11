e69d33c4c065f6b4ce48e3079a4292094c7c7258
WiseWoo <112747288+WiseWoo@users.noreply.github.com>
2024-03-11 14:59:56 +0900

    factor: 버그 수정

--- Extractor.py
+++ Extractor.py
@@ -112,9 +112,13 @@ def extractor():
         img = transform(img).unsqueeze(0)
         outputs = model(img)
         _, predicted = torch.max(outputs, 1)
-        target_url = "http://translate-service.fs-translate.svc.cluster.local/api/v1/translate"
+        target_url = "http://translate-service.fs-translate.svc.cluster.local/api/v1/translate/"
+        # "http://translate-service.fs-translate.svc.cluster.local/api/v1/translate/"
+        # target_url = target_url+"?name="+store_names[predicted.item()]
+        store_name = store_names[predicted.item()]
+        target_url += store_name  # 경로 수정
         store_names = ["찐퍼", "찐퍼", "찐퍼", "찐퍼"]
-        target_url = target_url+"?name="+store_names[predicted.item()]
+        
         
         # translate_response = requests.post(target_url, json={'name': predicted.item(), 'Language' : language})
         translate_response = requests.get(target_url, headers={'Language': language})
